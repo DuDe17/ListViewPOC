d62261c07f2121ed72facecf270b9c0ad530bad9
DuDe17 <shuklaasit@gmail.com>
2018-01-03 16:18:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashukla/listviewpoc/Item.java
@@ -0,0 +1,46 @@
+package com.example.ashukla.listviewpoc;
+
+/**
+ * Created by ashukla on 1/3/2018.
+ */
+
+public class Item {
+    private String details;
+    private String name;
+    private int price;
+
+    public Item(){
+
+    }
+
+    public Item(String i, String d, int p){
+        this.details = d;
+        this.name = i;
+        this.price = p;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
